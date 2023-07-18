8e6203a30ec6ddfd0998f9b32bc4806cca05492b
Mojid Eroje <93765911+Eroje@users.noreply.github.com>
2023-07-18 12:58:03 +0100

    Add More Files

---
+++ js/practice.js
@@ -0,0 +1,19 @@
+
+
+//document.getElementById("mainTitle");
+//console.log("Thisis an element of type: ", mainTitle.nodeType);
+//console.log("The inner HTML is ", mainTitle.innerHTML);
+//console.log("There are this many child objects ", mainTitle.childNodes);
+
+//var myLinks = document.getElementsByTagName("a");
+//var firstLink = myLinks[0].getAttribute("href");
+//console.log(firstLink);
+
+document.getElementById("mainTitle").innerHTML = "This is why you must plan your projects with SPATA!"
+document.getElementById("mainTitle").style.color = "blue";
+document.getElementById("works").setAttribute("style", "font-weight: bold");
+document.getElementById("works").style.color = "red";
+//var align = mainTitle.getAttribute("align");
+//console.log("align");
+
+//document.getElementsByClassName("about-section, bold-text").innerHTML = "This is why you must plan your projects with SPATA!"
